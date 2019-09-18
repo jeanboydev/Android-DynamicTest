97a6ef2c64b865a755bf8ad494c8b31c872b28e6
jeanboy <jeanboy@foxmail.com>
2019-09-18 17:04:06 +0800

    update

--- app/src/main/java/com/jeanboy/app/dynamictest/ui/MainActivity.java
+++ app/src/main/java/com/jeanboy/app/dynamictest/ui/MainActivity.java
@@ -8,11 +8,18 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.jeanboy.app.dynamictest.R;
+import com.jeanboy.app.dynamictest.patch.TinkerManager;
 import com.jeanboy.app.dynamictest.utils.BugUtil;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv_display;
+
+    private static final String FILE_STUFFIX = ".apk";
+    // /storage/emulated/0/Android/data/com.jeanboy.app.dynamictest/cache/patch/
+    private String patchDir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +38,20 @@ public class MainActivity extends AppCompatActivity {
 //                Log.e("======", "classloader:" + classLoader.toString());
 //            }
 //        }
+
+        patchDir = getExternalCacheDir().getAbsolutePath() + "/patch/";
+        Log.e("=====",patchDir);
+        File file = new File(patchDir);
+        if (!file.exists()) {
+            file.mkdir();
+        }
     }
 
     public void toHotFix(View view) {
+
+        String path = patchDir.concat("test").concat(FILE_STUFFIX);
+        Log.e("===path==",path);
+        TinkerManager.loadPatch(path);
     }
 
     public void toUpdate(View view) {
